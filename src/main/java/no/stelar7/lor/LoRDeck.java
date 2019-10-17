4b9b2e4883917fe15222d4c7a00d04699aa362c0
stelar7 <dudedbz@gmail.com>
2019-10-17 19:23:17 +0200

    Initial commit of Java code for LoRDeckCode

---
+++ src/main/java/no/stelar7/lor/LoRDeck.java
@@ -0,0 +1,229 @@
+package no.stelar7.lor;
+
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class LoRDeck
+{
+    private static Map<String, Integer> factionIds = new HashMap<>()
+    {{
+        put("DE", 0);
+        put("FR", 1);
+        put("IO", 2);
+        put("NX", 3);
+        put("PZ", 4);
+        put("SI", 5);
+    }};
+    
+    private static Map<Integer, String> IdFactions = new HashMap<>()
+    {{
+        put(0, "DE");
+        put(1, "FR");
+        put(2, "IO");
+        put(3, "NX");
+        put(4, "PZ");
+        put(5, "SI");
+    }};
+    
+    public static List<LorCardCount> decode(String code)
+    {
+        List<LorCardCount> result = new ArrayList<>();
+        List<Byte>         bytes  = new ArrayList<>(Arrays.asList(Base32.decodeBoxed(code)));
+        
+        int firstByte = bytes.remove(0);
+        int format    = firstByte >>> 4;
+        int version   = firstByte & 0xF;
+        
+        int MAX_KNOWN_VERSION = 1;
+        if (version > MAX_KNOWN_VERSION)
+        {
+            throw new IllegalArgumentException("The provided code requires a higher version of this library; please update");
+        }
+        
+        for (int i = 3; i > 0; i--)
+        {
+            int groupCount = VarInt.pop(bytes);
+            
+            for (int j = 0; j < groupCount; j++)
+            {
+                int itemCount = VarInt.pop(bytes);
+                int set       = VarInt.pop(bytes);
+                int faction   = VarInt.pop(bytes);
+                
+                for (int k = 0; k < itemCount; k++)
+                {
+                    int    card          = VarInt.pop(bytes);
+                    String setString     = padLeft(String.valueOf(set), 2);
+                    String factionString = IdFactions.get(faction);
+                    String cardString    = padLeft(String.valueOf(card), 3);
+                    
+                    result.add(new LorCardCount(setString, factionString, cardString, i));
+                }
+            }
+        }
+        
+        while (bytes.size() > 0)
+        {
+            int count   = VarInt.pop(bytes);
+            int set     = VarInt.pop(bytes);
+            int faction = VarInt.pop(bytes);
+            int number  = VarInt.pop(bytes);
+            
+            String setString     = padLeft(String.valueOf(set), 2);
+            String factionString = IdFactions.get(faction);
+            String numberString  = padLeft(String.valueOf(number), 3);
+            
+            result.add(new LorCardCount(setString, factionString, numberString, count));
+        }
+        
+        result.sort(Comparator.comparing(LorCardCount::getCount).reversed().thenComparing(LorCardCount::getCardCode));
+        
+        return result;
+        
+    }
+    
+    private static String padLeft(String input, int length)
+    {
+        return "0".repeat(length).substring(input.length()) + input;
+    }
+    
+    public static String encode(List<LorCardCount> cards)
+    {
+        List<Integer> result = new ArrayList<>();
+        if (!isValidDeck(cards))
+        {
+            throw new IllegalArgumentException("The deck provided contains invalid card codes");
+        }
+        
+        // format and version
+        result.add(17);
+        
+        Map<Integer, List<LorCardCount>> counts   = cards.stream().collect(Collectors.groupingBy(LorCardCount::getCount));
+        List<List<LorCardCount>>         grouped3 = groupByFactionAndSetSorted(counts.getOrDefault(3, new ArrayList<>()));
+        List<List<LorCardCount>>         grouped2 = groupByFactionAndSetSorted(counts.getOrDefault(2, new ArrayList<>()));
+        List<List<LorCardCount>>         grouped1 = groupByFactionAndSetSorted(counts.getOrDefault(1, new ArrayList<>()));
+        List<LorCardCount> nOfs = counts.entrySet().stream()
+                                        .filter(e -> e.getKey() > 3)
+                                        .flatMap(e -> e.getValue().stream())
+                                        .collect(Collectors.toList());
+        
+        result.addAll(encodeGroup(grouped3));
+        result.addAll(encodeGroup(grouped2));
+        result.addAll(encodeGroup(grouped1));
+        result.addAll(encodeNofs(nOfs));
+        
+        return Base32.encodeBoxed(result);
+    }
+    
+    private static List<Integer> encodeNofs(List<LorCardCount> nOfs)
+    {
+        List<Integer> result = new ArrayList<>();
+        
+        for (LorCardCount card : nOfs)
+        {
+            int faction = factionIds.get(card.getFaction());
+            
+            result.addAll(VarInt.get(card.getCount()));
+            result.addAll(VarInt.get(card.getSet()));
+            result.addAll(VarInt.get(faction));
+            result.addAll(VarInt.get(card.getId()));
+        }
+        
+        return result;
+    }
+    
+    private static List<Integer> encodeGroup(List<List<LorCardCount>> group)
+    {
+        List<Integer> result = new ArrayList<>(VarInt.get(group.size()));
+        
+        for (List<LorCardCount> list : group)
+        {
+            result.addAll(VarInt.get(list.size()));
+            LorCardCount first = list.get(0);
+            
+            int faction = factionIds.get(first.getFaction());
+            result.addAll(VarInt.get(first.getSet()));
+            result.addAll(VarInt.get(faction));
+            
+            for (LorCardCount card : list)
+            {
+                result.addAll(VarInt.get(card.getId()));
+            }
+        }
+        
+        return result;
+    }
+    
+    private static boolean isValidDeck(List<LorCardCount> cards)
+    {
+        for (LorCardCount card : cards)
+        {
+            if (card.getCardCode().length() != 7)
+            {
+                return false;
+            }
+            
+            try
+            {
+                // set and card id
+                Integer.parseInt(card.getCardCode().substring(0, 2));
+                Integer.parseInt(card.getCardCode().substring(4, 7));
+            } catch (NumberFormatException e)
+            {
+                return false;
+            }
+            
+            String faction = card.getCardCode().substring(2, 4);
+            if (!factionIds.containsKey(faction))
+            {
+                return false;
+            }
+            
+            
+            if (card.getCount() < 1)
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private static List<List<LorCardCount>> groupByFactionAndSetSorted(List<LorCardCount> cards)
+    {
+        List<List<LorCardCount>> result = new ArrayList<>();
+        
+        while (cards.size() > 0)
+        {
+            List<LorCardCount> set = new ArrayList<>();
+            
+            LorCardCount first = cards.remove(0);
+            set.add(first);
+            
+            for (int i = cards.size() - 1; i >= 0; i--)
+            {
+                LorCardCount compare = cards.get(i);
+                
+                if (first.getSet() == compare.getSet() && first.getFaction().equalsIgnoreCase(compare.getFaction()))
+                {
+                    set.add(compare);
+                    cards.remove(i);
+                }
+            }
+            
+            result.add(set);
+        }
+        
+        // sort outer list by size, then by inner list code, then sort inner list by code
+        Comparator<List<LorCardCount>> c  = Comparator.comparing(List::size);
+        Comparator<List<LorCardCount>> c2 = Comparator.comparing((List<LorCardCount> a) -> a.get(0).getCardCode()).reversed();
+        result.sort(c.thenComparing(c2));
+        for (List<LorCardCount> group : result)
+        {
+            group.sort(Comparator.comparing(LorCardCount::getCardCode));
+        }
+        
+        return result;
+    }
+}
---
+++ src/test/java/TestLorDeck.java
@@ -0,0 +1,358 @@
+import no.stelar7.lor.*;
+import org.junit.*;
+
+import java.io.InputStream;
+import java.util.*;
+
+public class TestLorDeck
+{
+    
+    @Test
+    public void testEncodingRecommendedDecks()
+    {
+        InputStream file = TestLorDeck.class.getClassLoader().getResourceAsStream("testdata.txt");
+        if (file == null)
+        {
+            throw new RuntimeException("Unable to load test file");
+        }
+        
+        List<String>             codes = new ArrayList<>();
+        List<List<LorCardCount>> decks = new ArrayList<>();
+        
+        try (Scanner scanner = new Scanner(file))
+        {
+            while (scanner.hasNextLine())
+            {
+                String line = scanner.nextLine();
+                codes.add(line);
+                
+                List<LorCardCount> deck = new ArrayList<>();
+                while (scanner.hasNextLine() && !(line = scanner.nextLine()).equalsIgnoreCase(""))
+                {
+                    String[] parts = line.split(":");
+                    deck.add(new LorCardCount(parts[1], Integer.parseInt(parts[0])));
+                }
+                decks.add(deck);
+            }
+        }
+        
+        for (int i = 0; i < decks.size(); i++)
+        {
+            System.out.println(codes.get(i));
+            String encoded = LoRDeck.encode(decks.get(i));
+            Assert.assertEquals("Decks are not equal", codes.get(i), encoded);
+            
+            List<LorCardCount> decoded = LoRDeck.decode(encoded);
+            Assert.assertTrue("Did not produce same deck when re-coded", checkSameDeck(decks.get(i), decoded));
+        }
+    }
+    
+    @Test
+    public void testDecodeEncode()
+    {
+        String DECK_CODE = "CEBAIAIFB4WDANQIAEAQGDAUDAQSIJZUAIAQCBIFAEAQCBAA";
+        
+        List<LorCardCount> deck   = LoRDeck.decode(DECK_CODE);
+        String             result = LoRDeck.encode(deck);
+        
+        Assert.assertEquals("Did not transform code to match!", DECK_CODE, result);
+    }
+    
+    
+    @Test
+    public void testSmallDeck()
+    {
+        List<LorCardCount> deck = new ArrayList<>()
+        {{
+            add(new LorCardCount("01DE002", 3));
+        }};
+        
+        String             code    = LoRDeck.encode(deck);
+        List<LorCardCount> decoded = LoRDeck.decode(code);
+        
+        Assert.assertTrue("Did not produce same deck when re-coded", checkSameDeck(deck, decoded));
+    }
+    
+    @Test
+    public void testLargeDeck()
+    {
+        List<LorCardCount> deck = new ArrayList<>()
+        {{
+            add(new LorCardCount("01DE002", 3));
+            add(new LorCardCount("01DE003", 3));
+            add(new LorCardCount("01DE004", 3));
+            add(new LorCardCount("01DE005", 3));
+            add(new LorCardCount("01DE006", 3));
+            add(new LorCardCount("01DE007", 3));
+            add(new LorCardCount("01DE008", 3));
+            add(new LorCardCount("01DE009", 3));
+            add(new LorCardCount("01DE010", 3));
+            add(new LorCardCount("01DE011", 3));
+            add(new LorCardCount("01DE012", 3));
+            add(new LorCardCount("01DE013", 3));
+            add(new LorCardCount("01DE014", 3));
+            add(new LorCardCount("01DE015", 3));
+            add(new LorCardCount("01DE016", 3));
+            add(new LorCardCount("01DE017", 3));
+            add(new LorCardCount("01DE018", 3));
+            add(new LorCardCount("01DE019", 3));
+            add(new LorCardCount("01DE020", 3));
+            add(new LorCardCount("01DE021", 3));
+        }};
+        
+        String             code    = LoRDeck.encode(deck);
+        List<LorCardCount> decoded = LoRDeck.decode(code);
+        
+        Assert.assertTrue("Did not produce same deck when re-coded", checkSameDeck(deck, decoded));
+    }
+    
+    
+    @Test
+    public void testMoreThan3Small()
+    {
+        List<LorCardCount> deck = new ArrayList<>()
+        {{
+            add(new LorCardCount("01DE002", 4));
+        }};
+        
+        String             code    = LoRDeck.encode(deck);
+        List<LorCardCount> decoded = LoRDeck.decode(code);
+        
+        Assert.assertTrue("Did not produce same deck when re-coded", checkSameDeck(deck, decoded));
+    }
+    
+    @Test
+    public void testMoreThan3Large()
+    {
+        List<LorCardCount> deck = new ArrayList<>()
+        {{
+            add(new LorCardCount("01DE002", 3));
+            add(new LorCardCount("01DE003", 3));
+            add(new LorCardCount("01DE004", 3));
+            add(new LorCardCount("01DE005", 3));
+            add(new LorCardCount("01DE006", 4));
+            add(new LorCardCount("01DE007", 5));
+            add(new LorCardCount("01DE008", 6));
+            add(new LorCardCount("01DE009", 7));
+            add(new LorCardCount("01DE010", 8));
+            add(new LorCardCount("01DE011", 9));
+            add(new LorCardCount("01DE012", 3));
+            add(new LorCardCount("01DE013", 3));
+            add(new LorCardCount("01DE014", 3));
+            add(new LorCardCount("01DE015", 3));
+            add(new LorCardCount("01DE016", 3));
+            add(new LorCardCount("01DE017", 3));
+            add(new LorCardCount("01DE018", 3));
+            add(new LorCardCount("01DE019", 3));
+            add(new LorCardCount("01DE020", 3));
+            add(new LorCardCount("01DE021", 3));
+        }};
+        
+        String             code    = LoRDeck.encode(deck);
+        List<LorCardCount> decoded = LoRDeck.decode(code);
+        
+        Assert.assertTrue("Did not produce same deck when re-coded", checkSameDeck(deck, decoded));
+    }
+    
+    @Test
+    public void testSingleCard40()
+    {
+        List<LorCardCount> deck = new ArrayList<>()
+        {{
+            add(new LorCardCount("01DE002", 40));
+        }};
+        
+        String             code    = LoRDeck.encode(deck);
+        List<LorCardCount> decoded = LoRDeck.decode(code);
+        
+        Assert.assertTrue("Did not produce same deck when re-coded", checkSameDeck(deck, decoded));
+    }
+    
+    @Test
+    public void testWorstCaseLength()
+    {
+        List<LorCardCount> deck = new ArrayList<>()
+        {{
+            add(new LorCardCount("01DE002", 4));
+            add(new LorCardCount("01DE003", 4));
+            add(new LorCardCount("01DE004", 4));
+            add(new LorCardCount("01DE005", 4));
+            add(new LorCardCount("01DE006", 4));
+            add(new LorCardCount("01DE007", 5));
+            add(new LorCardCount("01DE008", 6));
+            add(new LorCardCount("01DE009", 7));
+            add(new LorCardCount("01DE010", 8));
+            add(new LorCardCount("01DE011", 9));
+            add(new LorCardCount("01DE012", 4));
+            add(new LorCardCount("01DE013", 4));
+            add(new LorCardCount("01DE014", 4));
+            add(new LorCardCount("01DE015", 4));
+            add(new LorCardCount("01DE016", 4));
+            add(new LorCardCount("01DE017", 4));
+            add(new LorCardCount("01DE018", 4));
+            add(new LorCardCount("01DE019", 4));
+            add(new LorCardCount("01DE020", 4));
+            add(new LorCardCount("01DE021", 4));
+        }};
+        
+        String             code    = LoRDeck.encode(deck);
+        List<LorCardCount> decoded = LoRDeck.decode(code);
+        
+        Assert.assertTrue("Did not produce same deck when re-coded", checkSameDeck(deck, decoded));
+    }
+    
+    @Test
+    public void testOrderDoesNotMatter()
+    {
+        List<LorCardCount> deck = new ArrayList<>()
+        {{
+            add(new LorCardCount("01DE002", 1));
+            add(new LorCardCount("01DE003", 2));
+            add(new LorCardCount("02DE003", 3));
+        }};
+        
+        List<LorCardCount> deck2 = new ArrayList<>()
+        {{
+            add(new LorCardCount("01DE003", 2));
+            add(new LorCardCount("02DE003", 3));
+            add(new LorCardCount("01DE002", 1));
+        }};
+        
+        String code  = LoRDeck.encode(deck);
+        String code2 = LoRDeck.encode(deck2);
+        
+        Assert.assertEquals("Order matters?", code, code2);
+    }
+    
+    @Test
+    public void testOrderDoesNotMatterMoreThan3()
+    {
+        List<LorCardCount> deck = new ArrayList<>()
+        {{
+            add(new LorCardCount("01DE002", 4));
+            add(new LorCardCount("01DE003", 2));
+            add(new LorCardCount("02DE003", 3));
+            add(new LorCardCount("01DE004", 5));
+        }};
+        
+        List<LorCardCount> deck2 = new ArrayList<>()
+        {{
+            add(new LorCardCount("01DE004", 5));
+            add(new LorCardCount("01DE003", 2));
+            add(new LorCardCount("02DE003", 3));
+            add(new LorCardCount("01DE002", 4));
+        }};
+        
+        String code  = LoRDeck.encode(deck);
+        String code2 = LoRDeck.encode(deck2);
+        
+        Assert.assertEquals("Order matters?", code, code2);
+    }
+    
+    @Test
+    public void testInvalidDecks()
+    {
+        List<LorCardCount> deck = new ArrayList<>()
+        {
+            {
+                add(new LorCardCount("01DE02", 1));
+            }
+        };
+        checkDeck(deck);
+        
+        deck = new ArrayList<>()
+        {
+            {
+                add(new LorCardCount("01XX202", 1));
+            }
+        };
+        checkDeck(deck);
+        
+        deck = new ArrayList<>()
+        {
+            {
+                add(new LorCardCount("01DE002", 0));
+            }
+        };
+        checkDeck(deck);
+        
+        deck = new ArrayList<>()
+        {
+            {
+                add(new LorCardCount("01DE002", -1));
+            }
+        };
+        checkDeck(deck);
+    }
+    
+    @Test
+    public void testInvalidCodes()
+    {
+        String badNot32 = "This is not a card code. Dont @me";
+        String bad32    = "ABCDEFG";
+        String empty    = "";
+        
+        checkCode(badNot32);
+        checkCode(bad32);
+        checkCode(empty);
+    }
+    
+    private void checkCode(String code)
+    {
+        try
+        {
+            List<LorCardCount> deck = LoRDeck.decode(code);
+            Assert.fail("Invalid code did not produce an error");
+        } catch (IllegalArgumentException e)
+        {
+            // ok
+        } catch (Exception e)
+        {
+            Assert.fail("Invalid code produced the wrong exception");
+        }
+    }
+    
+    
+    private void checkDeck(List<LorCardCount> deck)
+    {
+        try
+        {
+            LoRDeck.encode(deck);
+            Assert.fail("Invalid deck did not produce an error");
+        } catch (IllegalArgumentException e)
+        {
+            // ok
+        } catch (Exception e)
+        {
+            Assert.fail("Invalid deck produced the wrong exception");
+        }
+    }
+    
+    private boolean checkSameDeck(List<LorCardCount> a, List<LorCardCount> b)
+    {
+        if (a.size() != b.size())
+        {
+            return false;
+        }
+        
+        for (LorCardCount bCard : b)
+        {
+            boolean found = false;
+            for (LorCardCount aCard : a)
+            {
+                if (aCard.equals(bCard))
+                {
+                    found = true;
+                    break;
+                }
+            }
+            
+            if (!found)
+            {
+                return false;
+            }
+            
+        }
+        return true;
+    }
+}
